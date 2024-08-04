1324df98387edbf6d3719cacecc2f64d4d1f74c2
Glenn Fiedler <glenn@networknext.com>
2024-08-04 17:35:32 -0400

    should stand on its own now in c

--- filter.c
+++ filter.c
@@ -1,8 +1,98 @@
 
 #include <assert.h>
 #include <memory.h>
+#include <string.h>
 
-void generate_pittle( uint8_t * output, const uint8_t * from_address, const uint8_t * to_address, uint16_t packet_length )
+#if !defined (LITTLE_ENDIAN ) && !defined(BIG_ENDIAN)
+
+  #ifdef __BYTE_ORDER__
+    #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+      #define LITTLE_ENDIAN 1
+    #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+      #define BIG_ENDIAN 1
+    #else
+      #error Unknown machine endianess detected. Please define LITTLE_ENDIAN or BIG_ENDIAN.
+    #endif // __BYTE_ORDER__
+
+  // Detect with GLIBC's endian.h
+  #elif defined(__GLIBC__)
+    #include <endian.h>
+    #if (__BYTE_ORDER == __LITTLE_ENDIAN)
+      #define LITTLE_ENDIAN 1
+    #elif (__BYTE_ORDER == __BIG_ENDIAN)
+      #define BIG_ENDIAN 1
+    #else
+      #error Unknown machine endianess detected. Please define LITTLE_ENDIAN or BIG_ENDIAN.
+    #endif // __BYTE_ORDER
+
+  // Detect with _LITTLE_ENDIAN and _BIG_ENDIAN macro
+  #elif defined(_LITTLE_ENDIAN) && !defined(_BIG_ENDIAN)
+    #define LITTLE_ENDIAN 1
+  #elif defined(_BIG_ENDIAN) && !defined(_LITTLE_ENDIAN)
+    #define BIG_ENDIAN 1
+
+  // Detect with architecture macros
+  #elif    defined(__sparc)     || defined(__sparc__)                           \
+        || defined(_POWER)      || defined(__powerpc__)                         \
+        || defined(__ppc__)     || defined(__hpux)      || defined(__hppa)      \
+        || defined(_MIPSEB)     || defined(_POWER)      || defined(__s390__)
+    #define BIG_ENDIAN 1
+  #elif    defined(__i386__)    || defined(__alpha__)   || defined(__ia64)      \
+        || defined(__ia64__)    || defined(_M_IX86)     || defined(_M_IA64)     \
+        || defined(_M_ALPHA)    || defined(__amd64)     || defined(__amd64__)   \
+        || defined(_M_AMD64)    || defined(__x86_64)    || defined(__x86_64__)  \
+        || defined(_M_X64)      || defined(__bfin__)
+    #define LITTLE_ENDIAN 1
+  #elif defined(_MSC_VER) && defined(_M_ARM)
+    #define LITTLE_ENDIAN 1
+  #else
+    #error Unknown machine endianess detected. Please define LITTLE_ENDIAN or BIG_ENDIAN.
+  #endif
+
+#endif
+
+#if !defined(BIG_ENDIAN)
+#define BIG_ENDIAN 0
+#endif // #if !defined(BIG_ENDIAN)
+
+#if !defined(LITTLE_ENDIAN)
+#define LITTLE_ENDIAN 0
+#endif // #if !defined(LITTLE_ENDIAN)
+
+typedef uint64_t fnv_t;
+
+inline void fnv_init( fnv_t * fnv )
+{
+    *fnv = 0xCBF29CE484222325;
+}
+
+inline void fnv_write( fnv_t * fnv, const uint8_t * data, size_t size )
+{
+    for ( size_t i = 0; i < size; i++ )
+    {
+        (*fnv) ^= data[i];
+        (*fnv) *= 0x00000100000001B3;
+    }
+}
+
+inline uint64_t fnv_finalize( fnv_t * fnv )
+{
+    return *fnv;
+}
+
+inline uint64_t bswap( uint64_t value )
+{
+#ifdef __GNUC__
+    return __builtin_bswap64( value );
+#else // #ifdef __GNUC__
+    value = ( value & 0x00000000FFFFFFFF ) << 32 | ( value & 0xFFFFFFFF00000000 ) >> 32;
+    value = ( value & 0x0000FFFF0000FFFF ) << 16 | ( value & 0xFFFF0000FFFF0000 ) >> 16;
+    value = ( value & 0x00FF00FF00FF00FF ) << 8  | ( value & 0xFF00FF00FF00FF00 ) >> 8;
+    return value;
+#endif // #ifdef __GNUC__
+}
+
+inline void generate_pittle( uint8_t * output, const uint8_t * from_address, const uint8_t * to_address, uint16_t packet_length )
 {
     assert( output );
     assert( from_address );
@@ -25,7 +115,7 @@ void generate_pittle( uint8_t * output, const uint8_t * from_address, const uint
     output[1] = 1 | ( ( 255 - output[0] ) ^ 113 );
 }
 
-void generate_chonkle( uint8_t * output, const uint8_t * magic, const uint8_t * from_address, const uint8_t * to_address, uint16_t packet_length )
+inline void generate_chonkle( uint8_t * output, const uint8_t * magic, const uint8_t * from_address, const uint8_t * to_address, uint16_t packet_length )
 {
     assert( output );
     assert( magic );
@@ -64,7 +154,7 @@ void generate_chonkle( uint8_t * output, const uint8_t * magic, const uint8_t * 
     output[14] = ( ( data[7] & 0xFE ) >> 1 ) + 17;
 }
 
-bool basic_packet_filter( const uint8_t * data, uint16_t packet_length )
+inline bool basic_packet_filter( const uint8_t * data, uint16_t packet_length )
 {
     if ( packet_length == 0 )
         return false;
@@ -123,7 +213,7 @@ bool basic_packet_filter( const uint8_t * data, uint16_t packet_length )
     return true;
 }
 
-bool advanced_packet_filter( const uint8_t * data, const uint8_t * magic, const uint8_t * from_address, const uint8_t * to_address, uint16_t packet_length )
+inline bool advanced_packet_filter( const uint8_t * data, const uint8_t * magic, const uint8_t * from_address, const uint8_t * to_address, uint16_t packet_length )
 {
     if ( data[0] == 0 ) // IMPORTANT: for passthrough packet type
         return true;
